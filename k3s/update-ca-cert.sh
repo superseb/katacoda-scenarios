42ba1397a7ce1c9f8436f4d74342657aa5814ecd
Sebastiaan van Steenis <mail@superseb.nl>
2021-10-26 10:05:07 +0200

    one liner

--- k3s/update-ca-cert.sh
+++ k3s/update-ca-cert.sh
@@ -1,2 +1,1 @@
-apt update && apt -y install ca-certificates
-sed -i -e 's=^mozilla/DST_Root_CA_X3.crt=!mozilla/DST_Root_CA_X3.crt=' /etc/ca-certificates.conf && update-ca-certificates
+apt update && apt -y install ca-certificates && sed -i -e 's=^mozilla/DST_Root_CA_X3.crt=!mozilla/DST_Root_CA_X3.crt=' /etc/ca-certificates.conf && update-ca-certificates
